b1d5635b9fef26d3004c159dd065f409da96db77
Madeline Witters <mmwitters@gmail.com>
2024-09-27 11:39:22 -0400

    updated app file to include wildfire data

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ import pandas as pd
 import plotly.express as px
 
 # Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+df = pd.read_csv('/Users/madelinewitters/wildfire_data/test_dataset.csv')
 
 # Initialize the app
 app = Dash()
 
 # App layout
 app.layout = [
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.scatter_geo(data_frame=df,
+                     lat=df['LATITUDE'],
+                     lon=df['LONGITUDE'], 
+                     size=df['FIRE_SIZE'],
+                     color=df['FIRE_SIZE'],
+                     color_continuous_scale='YlOrRd',
+                     scope='usa'))
 ]
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
 
 # Run the app
 if __name__ == '__main__':
